e91161e23d426abbab5feb92226318abc90858c3
kyungchan3007 <61735579+kyungchan3007@users.noreply.github.com>
2025-10-16 16:58:32 +0900

    feat:프론트 페이지 작업시 사용될 아이콘 추가

---
+++ src/assets/icons/button/closeButton.tsx
@@ -0,0 +1,29 @@
+import { SVGProps } from "react";
+
+export default function CloseButton(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="20" height="21" viewBox="0 0 20 21" fill="none" {...props}>
+      <g clip-path="url(#clip0_8454_739)">
+        <path
+          d="M15 5.5L5 15.5"
+          stroke="#9F9FAB"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M5 5.5L15 15.5"
+          stroke="#9F9FAB"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_8454_739">
+          <rect width="20" height="20" fill="white" transform="translate(0 0.5)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
---
+++ src/assets/icons/button/leftButton.tsx
@@ -0,0 +1,29 @@
+import { SVGProps } from "react";
+
+export default function LeftButton(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clip-path="url(#clip0_8454_736)">
+        <path
+          d="M12.5 5.5L7.5 10.5L12.5 15.5"
+          stroke="#9F9FAB"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_8454_736">
+          <rect width="20" height="20" fill="white" transform="translate(0 0.5)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
